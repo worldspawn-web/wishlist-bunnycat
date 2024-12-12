1fda5caa304eafb0132d20c91b55710390b5f8f6
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-12 20:14:40 +0300

    feat: wishlist types

---
+++ types/wish.ts
@@ -0,0 +1,27 @@
+export type WishCategory = 'Вкусняшка' | 'Путешествие' | 'Вещь' | 'Фильм/Сериал' | 'Игра' | 'Другое';
+
+export interface Wish {
+  id: string;
+  title: string;
+  image: string;
+  price?: number;
+  priority: 'low' | 'medium' | 'high';
+  platform?: 'wildberries' | 'ozon' | 'market' | 'other';
+  comment?: string;
+  author: 'cat' | 'bunny';
+  link: string;
+  completed: boolean;
+  category: WishCategory;
+  createdAt: Date;
+  completedBy?: 'cat' | 'bunny';
+}
+
+export interface UserStats {
+  completedWishes: number;
+  completedLastMonth: number;
+}
+
+export interface Stats {
+  cat: UserStats;
+  bunny: UserStats;
+}
