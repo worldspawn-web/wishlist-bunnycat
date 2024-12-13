561a96e085ad25f060240aa1f537eadfb1a6ff84
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-13 19:52:32 +0300

    refactor: utils, constants & styles

---
+++ utils/dateUtils.ts
@@ -0,0 +1,17 @@
+export function formatDate(date: Date): string {
+  const months = [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ];
+  return `${months[date.getMonth()]} ${date.getFullYear()}`;
+}
