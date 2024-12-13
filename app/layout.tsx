97b5f529241c16b016770b5c5ece56db2f4957f3
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-13 19:38:32 +0300

    fix: layout refactor while fixing unlimited spinner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,12 @@
-'use client';
-
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-import LoadingSpinner from '@/components/LoadingSpinner';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const handleStart = () => {
-      setIsLoading(true);
-      // will remove later, for testing and debugging purposes
-      setTimeout(() => {
-        setIsLoading(false);
-      }, Math.floor(Math.random() * 2000) + 1000);
-    };
-
-    handleStart();
-  }, [pathname]);
-
   return (
     <html lang="en">
-      <body className={`${inter.className} ${isLoading ? 'overflow-hidden' : ''}`}>
-        {isLoading && <LoadingSpinner />}
-        <div className={`transition-all duration-300 ${isLoading ? 'blur-sm' : ''}`}>{children}</div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
