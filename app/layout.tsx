710ca68d45936fb15d4cc573e8082bc916aed8f7
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-23 01:01:06 +0300

    fix: lazy load of unavailable en locale

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { locales } from '@/i18n';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
-
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="ru">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from 'next';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
   images: {
