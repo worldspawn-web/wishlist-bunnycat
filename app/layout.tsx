0a52eeb8478d44fc42fe2055cdddd0852e2d3bdb
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-23 00:54:30 +0300

    feat: fixed layout for possible future locales

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { locales } from '@/i18n';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html>
       <body className={inter.className}>{children}</body>
     </html>
   );
