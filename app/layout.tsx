4c6f0431febbf58fc82a4270c6de16bae4f3e077
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-26 00:23:19 +0300

    feat: theme provider integration (also removed hydration warning)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ru">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
