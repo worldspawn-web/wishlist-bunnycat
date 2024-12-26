19460d74da80a135d3a28a77c343c11070fd5f18
Michael Lozitckii <worldspawn.web@gmail.com>
2024-12-26 21:19:05 +0300

    feat: 404 page in case something happens

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,24 +1,37 @@
-import { ReactNode } from 'react';
-import { NextIntlClientProvider } from 'next-intl';
+import '../globals.css';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { Toaster } from '@/components/ui/toaster';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 import { notFound } from 'next/navigation';
 
-export default async function LocaleLayout({
+const inter = Inter({ subsets: ['latin'] });
+
+export function generateStaticParams() {
+  return [{ locale: 'ru' }];
+}
+
+export default function RootLayout({
   children,
   params: { locale },
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   params: { locale: string };
 }) {
-  let messages;
-  try {
-    messages = (await import(`../../locales/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
+  const messages = useMessages();
+
+  if (!messages) notFound();
 
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
-      {children}
-    </NextIntlClientProvider>
+    <html lang={locale} suppressHydrationWarning>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
